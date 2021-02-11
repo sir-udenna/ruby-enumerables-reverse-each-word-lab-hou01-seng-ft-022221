54d83f65b87a2ec4beb98306885ef006d93550e2
Udenna Nebeolisa <udennaboy@gmail.com>
2021-02-11 01:27:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,4 @@
 def reverse_each_word(sentence)
-  #sentence.split.collect {|word| word.reverse}.join(" ")
-  
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
 
-```
-def reverse_each_word(sentance)
-  array = sentance.split
-  array.collect do |array|
-    array.reverse!
-  end
-  array.join(" ")
-end
-```
